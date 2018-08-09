d17013f04e8198fc988c57125880867406e01900
Unknown <pablo.osorio@xmarts.com>
2018-08-09 00:38:15 -0500

    Modificación del campo serie en factura, modificación en formato de factura QWEB

--- models/models.py
+++ models/models.py
@@ -5,12 +5,13 @@ class plantillas_wni_invoice(models.Model):
     _inherit = 'account.invoice.line'
 
     serie= fields.Text('No Serie', compute='_get_serie')
+
     @api.one
     @api.depends('serie', 'origin', 'product_id')
     def _get_serie(self):
         cr = self.env.cr
         if self.origin:
-            sql = "select splot.name from stock_picking pi left join stock_move sp on sp.picking_id =pi.id  left join stock_move_line spl on spl.move_id=sp.id left join stock_production_lot splot on splot.id=spl.lot_id where  pi.origin='"+str(self.origin)+"' AND sp.product_id="+str(self.product_id.id)+" AND splot.name != 'None'"
+            sql = "select splot.name from stock_picking pi left join stock_move sp on sp.picking_id =pi.id  left join stock_move_line spl on spl.move_id=sp.id left join stock_production_lot splot on splot.id=spl.lot_id where  pi.origin='"+str(self.origin)+"' AND sp.product_id="+str(self.product_id.id)+" AND splot.name != 'None' AND spl.ordered_qty=spl.qty_done AND qty_done>0"
         else:
             ori = str(self.name)
             if(ori.find("Venta del ticket:")>=0):
@@ -18,7 +19,7 @@ class plantillas_wni_invoice(models.Model):
             else:
                 ori = str(self.invoice_id.origin)
             if ori != '':
-                sql = "select splot.name from stock_picking pi left join stock_move sp on sp.picking_id =pi.id	left join stock_move_line spl on spl.move_id=sp.id left join stock_production_lot splot on splot.id=spl.lot_id where  pi.origin='"+str(ori)+"' AND sp.product_id="+str(self.product_id.id)+" AND splot.name != 'None'"
+                sql = "select splot.name from stock_picking pi left join stock_move sp on sp.picking_id =pi.id	left join stock_move_line spl on spl.move_id=sp.id left join stock_production_lot splot on splot.id=spl.lot_id where  pi.origin='"+str(ori)+"' AND sp.product_id="+str(self.product_id.id)+" AND splot.name != 'None' AND spl.ordered_qty=spl.qty_done AND qty_done>0"
         cr.execute(sql)
         nseries = cr.fetchall()
         serie_text = ''
